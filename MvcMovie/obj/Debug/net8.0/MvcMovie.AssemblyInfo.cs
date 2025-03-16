40e1b2b8dda7baab967603bcfbd5566efb17cac4
YogendraSherpunja <yogendramagar15@gmail.com>
2025-03-15 20:00:36 -0400

    model added

--- MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8d7862feea3715caee91d04d68133235b010629")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f4eb72e0b3de3d210165878c2e20d38c8e028ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
